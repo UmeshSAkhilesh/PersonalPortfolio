b6aac24a03d0d176e814ba75f93c7ce8358589d5
Akhilesh <tiwariakhilesh412@gmail.com>
2021-12-17 17:35:26 +0530

    Initail commit

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+
+    //toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+});
+
+//Typing JavaScript
+
+var typed= new Typed(".typing",{
+    strings: [".Net Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+});
+
+var typed= new Typed(".typing-2",{
+    strings: [".Net Developer","WebvDeveloper"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+});
